7baae8e6734a4728d1d5e81a83e855fd9a20612c
Dave Goldsmith <davegoldsmith@hotmail.com>
2023-01-09 15:57:36 +0000

    Added first test for addRover()

---
+++ src/marsMissionControl.test.ts
@@ -0,0 +1,10 @@
+import { createRover, Rover } from "../src/marsMissionControl";
+describe("test addRover function", () => {
+  const rover1 = new Rover({x:0, y:0}, 'N', 'Rover1');
+  it("should return a new rover object", () => {
+    expect(createRover({x:0, y:0}, 'N')).toEqual(rover1);
+  });
+  // it("should return 5 for add(2,3)", () => {
+  //   expect(add(2, 3)).toBe(5);
+  // });
+});
